12cc1e8578d320effeca946df5f480a7bf59d297
bernaluis <bernal.lrm@hotmail.com>
2023-11-04 10:39:04 -0600

    Modelos

---
+++ src/main/java/com/parcial03/dw/models/Empleado.java
@@ -0,0 +1,42 @@
+package com.parcial03.dw.models;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Empleado {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = true, length = 100)
+    private String nombres;
+
+    @Column(nullable = true, length = 100)
+    private String apellidos;
+
+    @Column(nullable = true, length = 1)
+    private String genero;
+
+    @Column(nullable = true, length = 250)
+    private String direccion;
+
+    @OneToOne
+    @JoinColumn(name = "id_municipio", nullable = true, foreignKey = @ForeignKey(name = "FK_MUNI_EMPLE"))
+    private Municipio municipio;
+
+
+    @Column(nullable = true, length = 8)
+    private  String telefono;
+
+    @Column(nullable = true, length = 1)
+    private  String estado;
+
+    @Column(nullable = true, length = 250)
+    private  String motivo_inactivo;
+}
