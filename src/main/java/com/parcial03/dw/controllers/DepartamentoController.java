827b06d5ea9de4b031bb9414df42c42ba4a97c8c
Fernando Carranza <foreverfas_fc@hotmail.com>
2023-11-04 11:07:45 -0600

    :sparkles: create controllers

---
+++ src/main/java/com/parcial03/dw/controllers/DepartamentoController.java
@@ -0,0 +1,15 @@
+package com.parcial03.dw.controllers;
+
+import com.parcial03.dw.models.Departamento;
+import com.parcial03.dw.models.Municipio;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/departamentos")
+public class DepartamentoController {
+
+}
---
+++ src/main/java/com/parcial03/dw/controllers/EmpleadoController.java
@@ -0,0 +1,7 @@
+package com.parcial03.dw.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmpleadoController {
+}
