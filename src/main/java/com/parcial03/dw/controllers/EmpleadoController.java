ff5b1d24878553056699423a5ffc057d4291a0ef
Fernando Carranza <foreverfas_fc@hotmail.com>
2023-11-04 11:19:52 -0600

    :sparkles: add repository, repo and mapper dependency

--- src/main/java/com/parcial03/dw/controllers/EmpleadoController.java
+++ src/main/java/com/parcial03/dw/controllers/EmpleadoController.java
@@ -1,7 +1,10 @@
 package com.parcial03.dw.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/empleados")
 public class EmpleadoController {
+
 }
---
+++ src/main/java/com/parcial03/dw/services/ICRUD.java
@@ -0,0 +1,12 @@
+package com.parcial03.dw.services;
+
+import java.util.List;
+
+public interface ICRUD<T, ID> {
+
+    T save(T t);
+    T update(T t);
+    T findById(ID id);
+    List<T> findAll();
+    void delete(ID id);
+}
